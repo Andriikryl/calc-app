f03d2c4f63bcf86ff2849ed49b70346058f83b73
Amdrii <andreyk557@gmail.com>
2023-03-24 00:11:57 +0200

    feat[calc-style]

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ function App() {
   return (
     <div className="calculator-grid">
       <div className="output">
-        <div className="previous-operand"></div>
-        <div className="current-operand"></div>
+        <div className="previous-operand">21312</div>
+        <div className="current-operand">1234</div>
       </div>
       <button className="span-two">AC</button>
       <button>DEL</button>
